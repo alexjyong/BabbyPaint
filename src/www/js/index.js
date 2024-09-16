59442d630f77a1ac2571280ea62259a0e93fbbbb
Alex Yong <alexjyong@gmail.com>
2024-09-16 14:22:27 -0400

    Update index.js

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -155,7 +155,17 @@ lockButton.on('click', function() {
     }
 });
 
-// Clear canvas on button click
+// Clear canvas on 3 button click
 clearButton.on('click', function() {
+    var currentTime = Date.now();
+        if (currentTime - lastTap < 500) {
+            tapCount++;
+            if (tapCount >= 3) {
+                context.clearRect(0, 0, $canvas[0].width, $canvas[0].height);
+            }
+        } else {
+            tapCount = 1;
+        }
+        lastTap = currentTime;
     context.clearRect(0, 0, $canvas[0].width, $canvas[0].height);
 });
